a7b13d3626064f7c97783a53d99ed565fda73d52
Benjamin Geer <benjamin.geer@gmail.com>
2011-06-05 19:16:37 +0100

    Added simple-regression-plot in Python and Haskell

---
+++ haskell/simple-regression-plot/Farm.hs
@@ -0,0 +1,131 @@
+module Main where
+
+{-
+
+Reads x and y values from a two-column CSV file, does a least-squares
+linear regression and generates a gnuplot script that will plot the
+data as a scatterplot along with the regression line. Uses the Haskell
+wrapper for the GNU Scientific Library to do the linear
+regression. Generates the gnuplot script from a template using
+HStringTemplate. The generated gnuplot script uses gnuplot's epslatex
+terminal to produce LaTeX with Embedded PostScript, which can then be
+included in a LaTeX document. (I tried the Haskell gnuplot wrapper,
+but it doesn't support the epslatex terminal, and in any case this
+approach is actually much simpler and clearer.)
+
+-}
+
+import Data.List
+import Data.Maybe (maybe)
+import System.Environment (getArgs)
+import System.Console.GetOpt
+import Text.CSV.ByteString (CSV, parseCSV)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lex.Double as BD
+import qualified Data.Text as Text
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import qualified Data.Packed.Vector as Vector
+import qualified Numeric.GSL.Fitting.Linear as Linear
+import qualified Text.StringTemplate as Templ
+
+main = do
+  -- Get the command line args
+  args <- getArgs
+  let opts = getPlotOpts args
+  let dataFile = getDataFile opts
+  let templateFile = getTemplateFile opts
+  let scriptFile = getScriptFile opts
+  let outputFile = getOutputFile opts
+  
+  -- Read the data file
+  csvStr <- B.readFile dataFile
+  let (xs, ys) = case parseCSV csvStr of
+        Nothing -> error $ "Couldn't parse CSV file" ++ dataFile
+        Just csv -> csvToDoubles csv
+        
+  -- Do the calculation
+  let (intercept, slope) = linearRegression xs ys
+  
+  -- Generate a gnuplot script
+  let strAttrs = [ ("outputFile", outputFile),
+                   ("intercept", show intercept),
+                   ("slope", show slope), 
+                   ("dataFile", dataFile) ]
+  templateStr <- readFile templateFile
+  let template = Templ.setManyAttrib strAttrs (Templ.newSTMP templateStr)
+  writeFile scriptFile (Templ.render template)
+  
+  
+-- Command-line option processing
+  
+data Flag = DataFile String |
+            TemplateFile String |
+            ScriptFile String |
+            OutputFile String
+          deriving Show
+
+options :: [OptDescr Flag]
+options =
+  [ Option ['d']  ["data"]   (ReqArg DataFile "FILE") "data FILE",
+    Option ['t']  ["template"] (ReqArg TemplateFile "FILE") "template FILE",
+    Option ['s']  ["script"] (ReqArg ScriptFile "FILE") "script FILE",
+    Option ['o']  ["output"] (ReqArg OutputFile "FILE") "output FILE" ]
+
+getPlotOpts :: [String] -> [Flag]
+getPlotOpts args =
+  case getOpt Permute options args of
+    (o, [], []) -> o
+    (_, _, errs) -> usageError (concat errs)
+
+usageError s =
+  error (s ++ usageInfo header options)
+    where header = "Usage: Farm [OPTION...]"
+
+getDataFile :: [Flag] -> String
+getDataFile opts =
+  maybe (usageError "data filename required\n")
+  (\(DataFile val) -> val) (find finder opts)
+    where finder (DataFile _) = True
+          finder _ = False
+
+getTemplateFile :: [Flag] -> String
+getTemplateFile opts =
+  maybe (usageError "template filename required\n")
+  (\(TemplateFile val) -> val) (find finder opts)
+    where finder (TemplateFile _) = True
+          finder _ = False
+
+getScriptFile :: [Flag] -> String
+getScriptFile opts =
+  maybe (usageError "script filename required\n")
+  (\(ScriptFile val) -> val) (find finder opts)
+    where finder (ScriptFile _) = True
+          finder _ = False
+
+getOutputFile :: [Flag] -> String
+getOutputFile opts =
+  maybe (usageError "output filename required\n")
+  (\(OutputFile val) -> val) (find finder opts)
+    where finder (OutputFile _) = True
+          finder _ = False
+
+-- Parse the numbers in the data file
+csvToDoubles :: CSV -> ([Double], [Double])
+csvToDoubles csv =
+  foldr convRow ([], []) csv where
+    convRow row (xAcc, yAcc) = case row of
+      x:y:[] -> ((fieldToDouble x) : xAcc,
+                 (fieldToDouble y) : yAcc)
+      _ -> error "Expected two colums per row"
+      where fieldToDouble field =
+              case BD.readDouble field of
+                Nothing -> error $ "Couldn't parse field" ++
+                           (Text.unpack (decodeUtf8 field))
+                Just (value, _) -> value
+
+-- Fit the regression line
+linearRegression :: [Double] -> [Double] -> (Double, Double)
+linearRegression xs ys =
+  let (intercept, slope, cov00, cov01, cov11, chiSq) =
+        Linear.linear (Vector.fromList xs) (Vector.fromList ys)
+  in (intercept, slope)
---
+++ python/simple-regression-plot/farm.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+# Reads x and y values from a two-column CSV file, does a
+# least-squares linear regression using SciPy and plots the data as a
+# scatterplot along with the regression line. Uses Matplotlib's TeX
+# renderer to generate the plot as an Embedded PostScript file, which
+# can then be included in a LaTeX document.
+
+import matplotlib as mpl
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import csv
+import sys
+import getopt
+
+def main():
+    # Configure Matplotlib
+    config_mpl()
+
+    # Get the command-line options
+    data_file, output_file = get_options()
+
+    # Read the data
+    year, pop = read_data(data_file)
+
+    # Do the calculation
+    intercept, slope = linear_regression(year, pop)
+
+    # Generate the plot
+    plot(year, pop, intercept, slope, output_file)
+
+# Set some Matplotlib configuration parameters
+def config_mpl():
+    mpl.rc("text", usetex=True) # Use the TeX renderer
+    mpl.rc("font", family="serif")
+    mpl.rc("ps", usedistiller="xpdf") # Make scalable output
+    # mpl.rc("xtick.major", pad=8)
+    # mpl.rc("ytick.major", pad=8)
+
+# Command-line option processing
+def get_options():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],
+                                   "d:o:",
+                                   ["data=",
+                                    "output="])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
+
+    data_file = None
+    output_file = None
+
+    for o, a in opts:
+        if o in ("-d", "--data"):
+            data_file = a
+        elif o in ("-o", "--output"):
+            output_file = a
+        else:
+            assert False, "unhandled option"
+    
+    return data_file, output_file
+
+# Parse the numbers in the data file
+def read_data(data_file):
+    year = []
+    pop = []
+
+    try:
+        with open(data_file, "rb") as f:
+            reader = csv.reader(f)
+            
+            for row in reader:
+                row_year = int(row[0])
+                row_pop = float(row[1])
+                year.append(row_year)
+                pop.append(row_pop)
+    except IOError as (errno, strerror):
+        print "Error reading {0}: {1}" . format(filename, strerror)
+        sys.exit()
+    
+    return (year, pop)
+
+# Fit the regression line
+def linear_regression(year, pop):
+    slope, intercept, r, p, std_err = stats.linregress(year, pop)
+    return intercept, slope
+
+# Generate the plot
+def plot(year, pop, intercept, slope, output_file):
+    plt.axis(xmin = year[0], xmax = year[-1])
+    plt.xticks(year)
+    scatter_plot = plt.scatter(year, pop)
+    regression_line = plt.plot([year[0], year[-1]],
+                               [slope * x + intercept
+                                for x in [year[0], year[-1]]],
+                               'r-')
+    plt.xlabel("Year")
+    plt.ylabel("Millions of Persons")
+    plt.legend([scatter_plot, regression_line], ["Population", "Regression"])
+    plt.savefig(output_file)
+
+if __name__ == "__main__":
+    main()
