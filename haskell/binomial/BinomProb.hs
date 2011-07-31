fe23806939625d7ae324a19c84d32d2b168cb843
Benjamin Geer <benjamin.geer@gmail.com>
2011-07-31 18:51:50 +0100

    Added mean and standard deviation to binomial distributions.

--- haskell/binomial/BinomProb.hs
+++ haskell/binomial/BinomProb.hs
@@ -12,30 +12,36 @@ import Text.Printf (printf)
 
 {-
 
-Each child born to a particular set of parents has probability 0.25 of
-having blood type O. If these parents have 5 children, what is the
-probability that exactly 2 of them have type O blood?
+Problem 1: Each child born to a particular set of parents has
+probability 0.25 of having blood type O. If these parents have 5
+children, what is the probability that exactly 2 of them have type O
+blood?
 
 -}
 
-b1 = binomial 5 0.25
-p1 = probability b1 2
+p1Bin = binomial 5 0.25
+p1Prob = probability p1Bin 2
 
 {-
 
-A music distributor inspects an SRS of 10 CDs from a shipment of
-10,000 music CDs. Suppose that (unknown to the distributor) 10% of the
-CDs in the shipment have defective copy-protection schemes that will
-harm personal computers. The number X of CDs with defective copy
-protection has approximately the binomial distribution with n = 10 and
-p = 0.1. What is the probability that the sample contains no more than
-1 defective CD?
+Problem 2: A music distributor inspects an SRS of 10 CDs from a
+shipment of 10,000 music CDs. Suppose that (unknown to the
+distributor) 10% of the CDs in the shipment have defective
+copy-protection schemes that will harm personal computers. The number
+X of CDs with defective copy protection has approximately the binomial
+distribution with n = 10 and p = 0.1. What is the probability that the
+sample contains no more than 1 defective CD? What are the mean and
+standard deviation of this distribution?
 
 -}
 
-b2 = binomial 10 0.1
-p2 = cumulative b2 1
+p2Bin = binomial 10 0.1
+p2Prob = cumulative p2Bin 1
+p2Mean = mean p2Bin
+p2StdDev = sqrt (variance p2Bin)
 
 main = do
-  printf "Probability 1: %.4f\n" p1
-  printf "Probability 2: %.4f\n" p2
+  printf "Problem 1: probability %.4f\n\n" p1Prob
+  printf "Problem 2: probability %.4f\n" p2Prob
+  printf "           mean %.4f\n" p2Mean
+  printf "           standard dev. %.4f\n" p2StdDev
--- python/binomial/binomial.py
+++ python/binomial/binomial.py
@@ -2,25 +2,30 @@
 
 from scipy.stats import binom
 
-# Each child born to a particular set of parents has probability 0.25
-# of having blood type O. If these parents have 5 children, what is
-# the probability that exactly 2 of them have type O blood?
+# Problem 1: Each child born to a particular set of parents has
+# probability 0.25 of having blood type O. If these parents have 5
+# children, what is the probability that exactly 2 of them have type O
+# blood?
 
-p1 = binom.pmf(2, 5, 0.25)
+p1Prob = binom.pmf(2, 5, 0.25)
 
-# A music distributor inspects an SRS of 10 CDs from a shipment of
-# 10,000 music CDs. Suppose that (unknown to the distributor) 10% of
-# the CDs in the shipment have defective copy-protection schemes that
-# will harm personal computers. The number X of CDs with defective
-# copy protection has approximately the binomial distribution with n =
-# 10 and p = 0.1. What is the probability that the sample contains no
-# more than 1 defective CD?
+# Problem 2: A music distributor inspects an SRS of 10 CDs from a
+# shipment of 10,000 music CDs. Suppose that (unknown to the
+# distributor) 10% of the CDs in the shipment have defective
+# copy-protection schemes that will harm personal computers. The
+# number X of CDs with defective copy protection has approximately the
+# binomial distribution with n = 10 and p = 0.1. What is the
+# probability that the sample contains no more than 1 defective CD?
 
-p2 = binom.cdf(1, 10, 0.1)
+p2Prob = binom.cdf(1, 10, 0.1)
+p2Mean = binom.mean(10, 0.1)
+p2StdDev = binom.std(10, 0.1)
 
 def main():
-    print "Probability 1: %.4f" % p1
-    print "Probability 2: %.4f" % p2
+    print "Problem 1: probability %.4f\n" % p1Prob
+    print "Problem 2: probability %.4f" % p2Prob
+    print "           mean %.4f" % p2Mean
+    print "           standard dev. %.4f" % p2StdDev
 
 if __name__ == "__main__":
     main()
